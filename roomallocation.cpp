46e6bf99ef11b69ed026798ecadc67d1bc3d6c63
vstbls <7550178+vstbls@users.noreply.github.com>
2024-01-31 15:14:56 +0200

    Room Allocation solution

--- roomallocation.cpp
+++ roomallocation.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <tuple>
 
 using namespace std;
 
@@ -9,36 +10,37 @@ int main () {
     int n;
     cin >> n;
 
-    vector<pair<int,int>> times;
-    for (int _ = 0; _ < n; _++) {
+    vector<tuple<int,int,int>> times;
+    for (int i = 0; i < n; i++) {
         int a,b;
         cin >> a >> b;
-        times.push_back({a,b});
+        times.push_back({a,b,i});
     }
 
     sort(times.begin(), times.end());
 
     priority_queue<pair<int,int>> q;
     int max = 1;
-    vector<int> rooms;
+    vector<pair<int,int>> rooms;
     for (auto t: times) {
-        auto [a, b] = t;
+        auto [a, b, c] = t;
         if (q.empty()) {
             q.push({-b,1});
-            rooms.push_back(1);
+            rooms.push_back({c,1});
             continue;
         }
         if (a <= -q.top().first) {
             q.push({-b,q.size()+1});
-            rooms.push_back(q.size());
+            rooms.push_back({c,q.size()});
             if (q.size() > max) max = q.size();
         } else {
             int room = q.top().second;
             q.pop();
             q.push({-b, room});
-            rooms.push_back(room);
+            rooms.push_back({c,room});
         }
     }
+    sort(rooms.begin(), rooms.end());
     cout << max << "\n";
-    for (auto r: rooms) cout << r << " ";
+    for (auto r: rooms) cout << r.second << " ";
 }
