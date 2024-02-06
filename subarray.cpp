54ebd150efd5a8acb49dfed869490f5473c9c6a8
vstbls <7550178+vstbls@users.noreply.github.com>
2024-02-06 17:16:52 +0200

    Slow (and slightly wrong) Subarray

--- subarray.cpp
+++ subarray.cpp
@@ -14,19 +14,21 @@ int main () {
     for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
-        if (last = -1) {
+        if (last == -1) {
             last = 0;
             m[x] = 0;
             continue;
         }
-        if (m.count(x)) {
-            counter += i-m[last];
-        } else {
-            if (m.size() <= k) {
-                m[x] = i;
-                counter += i-last;
-            } else {
+        if (m.size() == k && !m.count(x)) {
+            pair<int,int> lowest = *m.begin();
+            for (auto p : m) {
+                if (p.second < lowest.first) lowest = p;
             }
+            last = lowest.second+1;
+            m.erase(lowest.first);
         }
+        m[x] = i;
+        counter += i-last+1;
     }
+    cout << counter;
 }
