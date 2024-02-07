c6bf9db586c8b940f092e1e1a5d80d68a69823c4
vstbls <7550178+vstbls@users.noreply.github.com>
2024-02-07 19:37:07 +0200

    Rectangle Cutting solution

--- rectanglecutting.cpp
+++ rectanglecutting.cpp
@@ -8,18 +8,26 @@ int main () {
     cin >> a >> b;
     if (a>b) swap(a,b);
 
-    vector<vector<int>> squares(a+1,vector<int>(b+1,0));
+    vector<vector<int>> cuts(a+1,vector<int>(b+1,0));
 
     for (int i = 1; i <= a; i++) {
         for (int j = i; j <= b; j++) {
-            int least = i*j;
-            for (int k = 1; k <= i; k++) {
-                int square_sum = squares[k][j-k] + squares[i-k][j] + 1;
-                if (square_sum < least) least = square_sum;
+            if (i == j) {
+                cuts[i][j] = 1;
+                continue;
             }
-            squares[i][j] = least;
-            if (j <= a) squares[j][i] = least;
+            int least = i*j;
+            for (int k = 1; k < i; k++) {
+                int cut_sum = cuts[k][j] + cuts[i-k][j];
+                if (cut_sum < least) least = cut_sum;
+            }
+            for (int k = 1; k < j; k++) {
+                int cut_sum = cuts[i][k] + cuts[i][j-k];
+                if (cut_sum < least) least = cut_sum;
+            }
+            cuts[i][j] = least;
+            if (j <= a) cuts[j][i] = least;
         }
     }
-    cout << squares[a][b]-1;
+    cout << cuts[a][b]-1;
 }
