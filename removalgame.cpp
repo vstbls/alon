ef7382c741488ab42f7d0bacf6b8066b8d6401a8
vstbls <7550178+vstbls@users.noreply.github.com>
2024-02-23 16:17:17 +0200

    Removal Game solution

--- removalgame.cpp
+++ removalgame.cpp
@@ -7,21 +7,21 @@ int main() {
     int n;
     cin >> n;
 
-    vector<vector<int>> pick(n, vector<int>(n, 0));
-    vector<vector<int>> wait = pick;
+    vector<vector<long>> pick(n, vector<long>(n, 0));
+    vector<vector<long>> wait = pick;
 
     for (int i = 0; i < n; i++) {
-        int x;
+        long x;
         cin >> x;
         pick[0][i] = x;
     }
 
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < n-i; j++) {
-            int left = pick[0][j] + wait[i-1][j+1];
-            int right = pick[0][j+i] + wait[i-1][j];
+            long left = pick[0][j] + wait[i-1][j+1];
+            long right = pick[0][j+i] + wait[i-1][j];
             pick[i][j] = max(left, right);
-            wait[i][j] = left > right ? pick[i-1][j+1] : pick[i-1][j];
+            wait[i][j] = min(pick[i-1][j+1], pick[i-1][j]);
         }
     }
 
